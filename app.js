fe128242347123cb00be8e0e74dfbafacc6e5625
Ridewitme <shcho@pnseng.com>
2025-04-19 22:47:58 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const root = document.getElementById("root");
+root.innerHTML = `
+  <div style="padding: 2rem; font-family: sans-serif;">
+    <h1 style="color: red;">Ride With Me</h1>
+    <p>진해에서 즐기는 감성 이륜차 렌트 서비스</p>
+    <ul>
+      <li>슈퍼커브</li>
+      <li>티맥스</li>
+    </ul>
+    <p>예약은 카카오톡, 구글폼, 인스타 DM으로!</p>
+  </div>
+`;
